5b9ccd65196e58929681230ab02baa6de2e44e7d
samocan <samocan1107@gmail.com>
2026-09-09 12:40:18 +0200

    A3.1 Tanksimulator btnZuruecksetzen implementiert

--- Tanksimulator/src/de/futurehome/tanksimulator/MyActionListener.java
+++ Tanksimulator/src/de/futurehome/tanksimulator/MyActionListener.java
@@ -30,5 +30,12 @@ public class MyActionListener implements ActionListener {
 				
 				f.lblFuellstand.setText("" + fuellstand);
 			}
+			if (obj == f.btnZuruecksetzen) {
+				double fuellstand = f.myTank.getFuellstand();
+				fuellstand = 0;
+				f.myTank.setFuellstand(fuellstand);
+				
+				f.lblFuellstand.setText("" + fuellstand);
+			}
 	}
 }
